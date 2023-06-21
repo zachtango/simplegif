33c33071e593a0d80733075f254880391871ea74
Zach Tang <zachtangoj@gmail.com>
2023-06-20 21:23:19 -0700

    basic project structure setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# GIF Screen Recorder
+Simple GIF screen recorder website that provides simple video editing as well
+
+
+
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import LandingPage from "./LandingPage";
+
+
+function App() {
+
+    
+    return (
+        <div className="App" class='w-full h-full'>
+            <LandingPage />
+        </div>
+    )
+}
+
+export default App;
