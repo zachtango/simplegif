0a297ed93e678f2aa1ab05fcba5089251e3cc79c
Zach Tang <zachtangoj@gmail.com>
2023-06-23 23:24:44 -0700

    chrome, safari, firefox codec support

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { detect } from 'detect-browser'
 
 import LandingPage from "./LandingPage/LandingPage";
 import RecordingPage from "./RecordingPage/RecordingPage";
@@ -7,7 +8,22 @@ import RecordingPage from "./RecordingPage/RecordingPage";
 import { PAGE_STATE } from "./utils/constants";
 import VideoEditingPage from "./VideoEditingPage/VideoEditingPage";
 
+const browser = detect();
+
 function App() {
+    
+    // Video mime type to use, default to vp8 (supported by Firefox)
+    let videoMimeType = 'video/webm; codecs=vp8';
+
+    switch (browser && browser.name) {
+      case 'chrome':
+        videoMimeType = 'video/webm; codecs=vp9'
+        break
+      case 'safari':
+        videoMimeType = 'video/mp4; codecs="avc1.42E01E"'
+        break;
+    }
+
     const [pageState, setPageState] = useState(PAGE_STATE.LANDING)
     const [videoBlob, setVideoBlob] = useState()
 
@@ -18,7 +34,8 @@ function App() {
             page = <LandingPage onStartRecording={() => setPageState(PAGE_STATE.RECORDING)} />
             break
         case PAGE_STATE.RECORDING:
-            page = <RecordingPage 
+            page = <RecordingPage
+                videoMimeType={videoMimeType}
                 onStopRecording={(blob) => {
                     setPageState(PAGE_STATE.EDITING)
                     setVideoBlob(blob)
